83edbfcfd7159f255f513b23838545d4306ef085
Moniroul Islam Russell <95379583+moniroul@users.noreply.github.com>
2025-02-08 16:07:13 +0600

    get post create

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ Route::get('/test', function () {
         'msg' => 'Api Working fine'
     ];
 });
+
+Route::get('/noteGet', NoteController::class  . '@index');
+Route::post('/noteAdd', NoteController::class  . '@store');
+
